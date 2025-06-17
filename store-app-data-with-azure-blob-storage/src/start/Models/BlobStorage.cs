ca8342471421fdaa24dd4f36e5a0d1c03e060988
ckiick1234 <ckiick1234@gmail.com>
2025-06-17 15:19:15 -0700

    Updated blobl methods

--- store-app-data-with-azure-blob-storage/src/start/Models/BlobStorage.cs
+++ store-app-data-with-azure-blob-storage/src/start/Models/BlobStorage.cs
@@ -22,25 +22,53 @@ namespace FileUploader.Models
         {
             BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
-            return containerClient.CreateIfNotExistsAsync();
+            return containerClient.CreateIfNotExistsAsync(); // To repeat, this will cerate a blob where one does not exsist.
         }
 
         public Task Save(Stream fileStream, string name)
         {
-            // Add Save code here
-            throw new NotImplementedException();
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+
+            // Get the container (folder) the file will be saved in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+
+            // Get the Blob Client used to interact with (including create) the blob
+            BlobClient blobClient = containerClient.GetBlobClient(name);
+
+            // Upload the blob
+            return blobClient.UploadAsync(fileStream);
         }
 
-        public Task<IEnumerable<string>> GetNames()
+        public async Task<IEnumerable<string>> GetNames()
         {
-            // Add GetNames code here
-            throw new NotImplementedException();
+            List<string> names = new List<string>();
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+
+            // Get the container the blobs are saved in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+
+            // This gets the info about the blobs in the container
+            AsyncPageable<BlobItem> blobs = containerClient.GetBlobsAsync();
+
+            await foreach (var blob in blobs)
+            {
+                names.Add(blob.Name);
+            }
+            return names;
         }
 
         public Task<Stream> Load(string name)
         {
-            // Add Load code here
-            throw new NotImplementedException();
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+
+            // Get the container the blobs are saved in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+
+            // Get a client to operate on the blob so we can read it.
+            BlobClient blobClient = containerClient.GetBlobClient(name);
+
+            return blobClient.OpenReadAsync();
         }
     }
 }
